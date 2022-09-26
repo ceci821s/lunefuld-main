f59e4145f0f2ade0a91131ad4a37d65235e1ecdf
Cecilie Dinsen <ceci821s@stud.kea.dk>
2022-09-26 13:51:39 +0200

    rettelser

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#BDCBC9",
+        green: "#ABBB6C",
+        lightgreen: "#C8DAAC",
+      },
+      fontFamily: {
+        sil: "Abyssinica SIL, serif",
+        lora: "Lora, serif;",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
